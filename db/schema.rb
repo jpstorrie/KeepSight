49b883a11181313d44341a02eead2bb460464142
Josiah Storrie <jpstorrie@gmail.com>
2023-03-14 14:45:31 -0600

    chore: added secure password to user in db

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_190848) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.string "email"
     t.boolean "recieve_emails"
     t.datetime "created_at", null: false
